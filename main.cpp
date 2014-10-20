ed1c74bcbb06bebaf65b715c42d2f6003524bc31
Grant Mercer <Gmercer015@gmail.com>
2014-10-20 00:16:56 -0700

    example written in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include <vector>
 #include "MassObject"
 
 int main()
 {
-  std::cout << "mass object program";
+  std::vector<MassObject> two_objects = {MassObject(0.0f, 0.0f, 0.0f, 5.9722e+24f), MassObject(6378.1e3f, 0.0f, 0.0f, 1.0f)};
+
+  float f = two_objects[0].gravitationalForce(two_objects[1]);
+  std::cout << "Force between two objects: " << f << std::endl;
   return 0;
 }
