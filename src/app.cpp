0f9c8eb233bad1a981ea78c5d1478ba80ca15581
Grant Mercer <Gmercer015@gmail.com>
2014-12-18 18:08:34 -0800

    Documentation written for app class

--- src/app.cpp
+++ src/app.cpp
@@ -16,56 +16,67 @@ bool App::running=true;
 
 App::App(sf::VideoMode mode)
 {
-  d_main_window.create(mode,
-    "Planet Simulator",
-    sf::Style::Default,
-    sf::ContextSettings(32, 8, 0, 3,3 )
-  );
+	//create main window
+	d_main_window.create(mode,
+		"Planet Simulator",
+		sf::Style::Default,
+		sf::ContextSettings(32, 8, 0, 3,3 )
+	);
 
-  d_main_window.setPosition(sf::Vector2i(500,50));
+	//set position of window to center of screen
+	d_main_window.setPosition(sf::Vector2i(500,50));
 }
 
 App::~App()
 {
+	//free dynamic memory and program id
 	delete d_planet_manager;
 	glDeleteProgram(d_program_id);
 }
 
 bool App::initGL(const int& planet_count)
 {
+	//ensure GLEW is enabled
 	glewExperimental = true;
 	if(glewInit() != GLEW_OK) {
 		std::cout << "Failed to initialize GLEW\n";
 		return false;
 	}
 
+	//initialize planet manager
 	d_planet_manager = new PlanetManager(&d_main_window, planet_count);
 
+	//generate OpenGL buffers for planets and clear screen
 	d_planet_manager->genGLBuffers();
 	glClearColor(0.0f, 0.0f, 0.2f, 0.0f);
 
 	d_program_id = LoadShaders("shaders/vertexshader.vert",
 		"shaders/fragmentshader.frag");
 
+	//These will be the vectors and matrices used in the shaders
 	d_cameraright_worldspace_id = glGetUniformLocation(d_program_id, "CameraRight_worldspace");
 	d_cameraup_worldspace_id = glGetUniformLocation(d_program_id, "CameraUp_worldspace");
 	d_viewprojmatrix_id = glGetUniformLocation(d_program_id, "VP");
 
+	//get our texture as well
 	d_texture_id = glGetUniformLocation(d_program_id, "myTextureSampler");
 	return true;
 }
 
 bool App::initPlanets(const std::string& texture_path)
 {
+	//simple load texture
 	if(!(d_planet_manager->loadTexture(texture_path.c_str())))
 		return false;
+	//call mamanger to initialize planets
 	d_planet_manager->initPlanets();
 	return true;
 }
 
 void App::run()
 {
-    while( running )
+	//begin main loop of program
+    while(running)
     {
         float delta = clock.restart().asSeconds();
         //handle window events
@@ -80,28 +91,33 @@ void App::run()
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		//TODO: remove user input, we don't want this shit moving
 		computeMatricesFromInputs(d_main_window, delta);
 		glm::mat4 projection_matrix = getProjectionMatrix();
 		glm::mat4 view_matrix = getViewMatrix();
 		glm::mat4 view_projection_matrix = projection_matrix * view_matrix;
 
+		//update our planets positions and speed
 		d_planet_manager->updatePlanets(delta, projection_matrix, view_matrix);
 
+		//TODO: remove blending, there will be collision detection soon
 		glEnable(GL_BLEND); 
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
+		//use our program
 		glUseProgram(d_program_id);
 
 		d_planet_manager->activateTexture();
-
 		glUniform1i(d_texture_id, 0);
 
+		//give the shaders what we promised here
 		glUniform3f(d_cameraright_worldspace_id, view_matrix[0][0], view_matrix[1][0], view_matrix[2][0]);
 		glUniform3f(d_cameraup_worldspace_id, view_matrix[0][1], view_matrix[1][1], view_matrix[2][1]);
-
 		glUniformMatrix4fv(d_viewprojmatrix_id, 1, GL_FALSE, &view_projection_matrix[0][0]);
 
+		//call mamanger to render planets
 		d_planet_manager->drawPlanets();
+		//display results to screen
         d_main_window.display();
     }
 }
--- src/app.hpp
+++ src/app.hpp
@@ -7,30 +7,49 @@
 
 class App{
   public:
+	//TODO? make constructor call initGL and initPlanets by itself
+	//The constructor of App does not actually do much other than
+	//	create the main window and set it's position to the middle
+	//  of the screen.
     App(sf::VideoMode mode);
     ~App();
 
+	//Initialize all openGL related components for the program, this
+	//	function will be called before initPlanets, as it creates the
+	//	foundation of the program. It will clear the screen, ensure GLEW
+	//	is working, create buffers, load shaders and more.
 	bool initGL(const int& planet_count);
 
+	//Initialize the initial state and values of all planets
+	//	that are too be rendered on the screen in the first frame
+	//	of the program. This function does very little itself, but
+	//	calls the manager's `initPlanets` function which sets the 
+	//	initial position, size, color, etc. of each planet.
 	bool initPlanets(const std::string& texture_path);
 
+	//The main loop of the entire program, run is responsible for
+	//	handling window events, receiving user input, calculating the
+	//	delta time for movement and render our planets to the screen.
+	//	The internal position calculation and such is done inside of 
+	//	the planet manager class.
     void run();
 
   private:
-	sf::Clock clock;
-    sf::Window d_main_window;
+	sf::Clock clock;						// calculate delta time
+    sf::Window d_main_window;				// main window of program
 
-	PlanetManager *d_planet_manager;
+	PlanetManager *d_planet_manager;		// our planet mamanger, dynamically allocated
 
-	GLuint d_program_id;
-	GLuint d_cameraright_worldspace_id;
-	GLuint d_cameraup_worldspace_id;
-	GLuint d_viewprojmatrix_id;
+	GLuint d_program_id;					// only one OpenGL program is used
+	GLuint d_cameraright_worldspace_id;		// vector to be passed into the shader
+	GLuint d_cameraup_worldspace_id;		// additional vector to be passed to shader
+	GLuint d_viewprojmatrix_id;				// view projection matrix to be passed to shader
 
-	GLuint d_texture_id;
+	GLuint d_texture_id;					// id of the texture(s)
 
 
-    static bool running;
+    static bool running;					// we only ever want one program running,
+											// unless you like making things difficult
 };
 
 #endif
